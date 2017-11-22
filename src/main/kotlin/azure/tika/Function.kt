f0989894458778bf69990aa33dd1bda09005dccd
Duncan Dickinson <duncan@dickinson.name>
2017-11-22 19:07:37 +1000

    Starting off Tika function

--- src/main/kotlin/azure/tika/Function.kt
+++ src/main/kotlin/azure/tika/Function.kt
@@ -3,6 +3,12 @@ package azure.tika
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.KotlinJsonAdapterFactory
 import com.squareup.moshi.Moshi
+import org.apache.tika.io.TikaInputStream
+import org.apache.tika.parser.AutoDetectParser
+import org.apache.tika.sax.BodyContentHandler
+import org.apache.tika.metadata.Metadata
+import org.apache.tika.parser.ParseContext
+import java.io.File
 
 data class GreetingResponse(val response: String = "")
 
@@ -22,4 +28,21 @@ fun greeting(name: String? = "World"): String {
     )
 }
 
-fun main(args: Array<String>) = println(greeting())
+fun tika(content: ByteArray): String {
+    val parser = AutoDetectParser()
+    val handler = BodyContentHandler()
+    val metadata = Metadata()
+    val parseContext = ParseContext()
+
+    val stream = TikaInputStream.get(content.inputStream())
+    parser.parse(stream, handler, metadata, parseContext)
+
+    return metadata.get("Content-Type")
+}
+
+fun main(args: Array<String>) {
+    if (args.size != 1) throw IllegalArgumentException("Require 1 input parameter")
+
+    println(tika(File(args[0]).readBytes()))
+
+}
